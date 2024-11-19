d50df177fc61bc8327f04c9bb476739ee5f5102c
subhasish442003 <bsubhasish44@gmail.com>
2024-11-19 10:49:17 +0530

    Create 08recog-synthspeech.py

---
+++ 08recog-synthspeech.py
@@ -0,0 +1,3 @@
+#Enviroment (.env) file for Recognize and synthesize speech
+SPEECH_KEY="7hovYdNPnGAdH7jhjOHs3DjyZrr2YXiZ36gLxDLLtMiBskcxGF9oJQQJ99AKACYeBjFXJ3w3AAAYACOGJbi4"
+SPEECH_REGION="eastus"
